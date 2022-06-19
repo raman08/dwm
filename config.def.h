298d1839558c3f729ce01f3e309f9b76462d85e4
Raman Preet Singh <ramanpre0810@gmail.com>
2022-06-19 18:14:01 +0530

    Added Center Patch

--- config.def.h
+++ config.def.h
@@ -39,8 +39,10 @@ static const Rule rules[] = {
 	{ "obs",                NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Lutris",             NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "firefox",   		NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
-	{ "Brave-browser",   	NULL,     NULL,           0,   	     0,          0,          -1,        -1 },
+	{ "Brave-browser",   	NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
 	{ "St",                 NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "Spotify",            NULL,     NULL,           1 << 4,    0,          0,           0,        -1 },
+	{ "Spotify Free",       NULL,     NULL,           1 << 4,    0,          0,           0,        -1 },
 	{ NULL,                 NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
